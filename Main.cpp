47b8b04c023db4e6c12947766ba016394ba4e69d
Chris <christophermartinezpromo17@gmail.com>
2025-04-21 21:23:36 -0600

    Basic lighting was implemented

--- Main.cpp
+++ Main.cpp
@@ -18,22 +18,65 @@ const unsigned int width = 800;
 const unsigned int height = 800;
 
 // Vertice coordinates
-GLfloat vertices[] = {
-    //Coordinates           Colors               Texture Coordinates
-    -0.5f,  0.0f,  0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 0.0f,  // Lower left corner
-    -0.5f,  0.0f, -0.5f,    0.83f, 0.70f, 0.44f,    5.0f, 0.0f,  // Upper left corner
-     0.5f,  0.0f, -0.5f,    0.83f, 0.70f, 0.44f,    0.0f, 0.0f,  // Upper right corner
-     0.5f,  0.0f,  0.5f,    0.83f, 0.70f, 0.44f,    5.0f, 0.0f,  // Lower right corner
-     0.0f,  0.8f,  0.0f,    0.92f, 0.86f, 0.76f,    2.5f, 5.0f   // Lower right corner
+// Vertices coordinates
+GLfloat vertices[] =
+{   //     COORDINATES     /        COLORS       /   TexCoord  /        NORMALS       //
+	-0.5f, 0.0f,  0.5f,    0.83f, 0.70f, 0.44f,   0.0f, 0.0f,   0.0f, -1.0f,  0.0f, // Bottom side
+	-0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,   0.0f, 5.0f,   0.0f, -1.0f,  0.0f, // Bottom side
+	 0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,   5.0f, 5.0f,   0.0f, -1.0f,  0.0f, // Bottom side
+	 0.5f, 0.0f,  0.5f,    0.83f, 0.70f, 0.44f,   5.0f, 0.0f,   0.0f, -1.0f,  0.0f, // Bottom side
+
+	-0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,   0.0f, 0.0f,  -0.8f,  0.5f,  0.0f, // Left Side
+	-0.5f, 0.0f,  0.5f,    0.83f, 0.70f, 0.44f,   5.0f, 0.0f,  -0.8f,  0.5f,  0.0f, // Left Side
+	 0.0f, 0.8f,  0.0f,    0.92f, 0.86f, 0.76f,   2.5f, 5.0f,  -0.8f,  0.5f,  0.0f, // Left Side
+
+	-0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,   5.0f, 0.0f,   0.0f,  0.5f, -0.8f, // Non-facing side
+	 0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,   0.0f, 0.0f,   0.0f,  0.5f, -0.8f, // Non-facing side
+	 0.0f, 0.8f,  0.0f,    0.92f, 0.86f, 0.76f,   2.5f, 5.0f,   0.0f,  0.5f, -0.8f, // Non-facing side
+
+	 0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,   0.0f, 0.0f,   0.8f,  0.5f,  0.0f, // Right side
+	 0.5f, 0.0f,  0.5f,    0.83f, 0.70f, 0.44f,   5.0f, 0.0f,   0.8f,  0.5f,  0.0f, // Right side
+	 0.0f, 0.8f,  0.0f,    0.92f, 0.86f, 0.76f,   2.5f, 5.0f,   0.8f,  0.5f,  0.0f, // Right side
+
+	 0.5f, 0.0f,  0.5f,    0.83f, 0.70f, 0.44f,   5.0f, 0.0f,   0.0f,  0.5f,  0.8f, // Facing side
+	-0.5f, 0.0f,  0.5f,    0.83f, 0.70f, 0.44f,   0.0f, 0.0f,   0.0f,  0.5f,  0.8f, // Facing side
+	 0.0f, 0.8f,  0.0f,    0.92f, 0.86f, 0.76f,   2.5f, 5.0f,   0.0f,  0.5f,  0.8f  // Facing side
 };
 
 GLuint indices[] = {
-	0, 1, 2, // Upper triangle
-	0, 2, 3, // Lower triangle
-	0, 1, 4, // Left triangle
-	1, 2, 4, // Upper triangle
-	2, 3, 4, // Right triangle
-	3, 0, 4  // Lower triangle
+	0, 1, 2,	// Botton side
+	0, 2, 3,	// Botton side
+	4, 6, 5,	// Left side
+	7, 9, 8,	// Non-facing side
+	10, 11, 12,	// Right side
+	13, 15, 14,	// Facing side
+};
+
+GLfloat lightVertices[] = {
+	// Coordinates
+	-0.1f, -0.1f,  0.1f,
+	-0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f,  0.1f,
+	-0.1f,  0.1f,  0.1f,
+	-0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f,  0.1f
+};
+
+GLuint lightIndices[] = {
+	0, 1, 2,
+	0, 2, 3,
+	0, 4, 7,
+	0, 7, 3,
+	3, 7, 6,
+	3, 6, 2,
+	2, 6, 5,
+	2, 5, 1,
+	1, 5, 4,
+	1, 4, 0,
+	4, 5, 6,
+	4, 6, 7
 };
 
 int main() {
@@ -80,13 +123,50 @@ int main() {
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO attributes such as coordinates and colors to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 11 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 11 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 11 * sizeof(float), (void*)(6 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 3, 3, GL_FLOAT, 11 * sizeof(float), (void*)(8 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
+
+	// Shader for the light cube 
+	Shader lightShader("light.vert", "light.frag");
+	VAO lightVAO;
+	lightVAO.Bind();
+
+	// Generate vertex buffer object and links it to vertices
+	VBO lightVBO(lightVertices, sizeof(lightVertices));
+	// Generate element buffer object and links it to indices
+	EBO lightEBO(lightIndices, sizeof(lightIndices));
+
+	// Links VBO attributes such as coordinates and colors to VAO
+	lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
+
+	// Unbind all to prevent accidentally modifying them
+	lightVAO.Unbind();
+	lightVBO.Unbind();
+	lightEBO.Unbind();
+
+	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
+
+	glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 pyramidModel = glm::mat4(1.0f);
+	pyramidModel = glm::translate(pyramidModel, pyramidPos);
+
+	lightShader.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+	glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	shaderProgram.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightColor.x, lightColor.y, lightColor.z);
 
 	// Textures
 	Texture popCat("pop_cat_.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
@@ -103,13 +183,18 @@ int main() {
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// Clean the back buffer and depth buffer and assigns the new color to it
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		// Tell OpenGL which shader program we want to use
-		shaderProgram.Activate();
 
 		// Handles camera inputs
 		camera.Inputs(window); // Take care of camera inputs
 		// Updates and exports the camera matrix to the Vertex Shader
-		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
+
+		// Tell OpenGL which shader program we want to use
+		shaderProgram.Activate();
+		// Exports the camera Position to the Fragment Shader for the specular lighting
+		glUniform3f(glGetUniformLocation(shaderProgram.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
+		// Exports the camMatrix to the Vertex Shader of the pyramid
+		camera.Matrix(shaderProgram, "camMatrix");
 
 		// Bind the texture so that appears in rendering 
 		popCat.Bind();
@@ -118,6 +203,16 @@ int main() {
 		// Drawn the triangule using the GL_TRIANGLES primitive
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
+
+		// Tells OpenGL to use the light shader program we want to use
+		lightShader.Activate();
+		// Exports the camera Position to the Vertex Shader of the light cube
+		camera.Matrix(lightShader, "camMatrix");
+		// Bind the VAO so OpenGL knows to use it
+		lightVAO.Bind();
+		// Draw primitives , number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
 		// Swap to the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
@@ -130,6 +225,11 @@ int main() {
 	EBO1.Delete();
 	popCat.Delete();
 	shaderProgram.Delete();
+
+	lightVAO.Delete();
+	lightVBO.Delete();
+	lightEBO.Delete();
+	lightShader.Delete();
 
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
--- default.frag
+++ default.frag
@@ -1,14 +1,41 @@
 #version 330 core
 out vec4 FragColor;
 
-// Input the color from vertex shader
+// Imports the color from vertex shader
 in vec3 color;
-// Input the texture coordinates from vertex shader
+// Imports the texture coordinates from vertex shader
 in vec2 texCoord;
+// Imports the normal from the vertex shader
+in vec3 Normal;
+// Imports the current position from the vertex shader
+in vec3 crntPos;
+
 // Gets the texture unit from the main function
 uniform sampler2D tex0;
+// Gets the light color from the main function
+uniform vec4 lightColor;
+// Gets the light position from the main function
+uniform vec3 lightPos;
+// Gets the camera position from the main function
+uniform vec3 camPos;
 
 void main()
 {
-	FragColor = texture(tex0, texCoord);
+	// Ambient lighting
+	float ambient = 0.20f;
+
+	// Diffuse lighting
+	vec3 normal = normalize(Normal);
+	vec3 lightDirection = normalize(lightPos - crntPos);
+	float diffuse = max(dot(normal, lightDirection), 0.0f);
+
+	// Specular lighting
+	float specularLight = 0.50f;
+	vec3 viewDirection = normalize(camPos - crntPos);
+	vec3 reflectDirection = reflect(-lightDirection, normal);
+	float specAmount = pow(max(dot(viewDirection, reflectDirection), 0.0f), 8);
+	float specular = specularLight * specAmount;
+
+	// Outputs final color
+	FragColor = texture(tex0, texCoord) * lightColor * (diffuse + ambient + specular);
 };
